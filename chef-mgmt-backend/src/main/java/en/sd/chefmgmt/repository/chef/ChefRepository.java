4f72450c947ea374063c091858036121a4d9b26e
sergiublaj <sergiublaj01@gmail.com>
2026-03-24 10:07:39 +0200

    fixed n + 1

--- chef-mgmt-backend/src/main/java/en/sd/chefmgmt/repository/chef/ChefRepository.java
+++ chef-mgmt-backend/src/main/java/en/sd/chefmgmt/repository/chef/ChefRepository.java
@@ -1,12 +1,20 @@
 package en.sd.chefmgmt.repository.chef;
 
 import en.sd.chefmgmt.model.ChefEntity;
+import org.jspecify.annotations.NonNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.UUID;
 
 public interface ChefRepository extends JpaRepository<ChefEntity, UUID>, JpaSpecificationExecutor<ChefEntity> {
+
+    @EntityGraph(attributePaths = "orders")
+    Page<ChefEntity> findAll(@NonNull Specification<ChefEntity> spec, @NonNull Pageable pageable);
 
     boolean existsByEmail(String email);
 
